e983ba7bbccb12cc60703e5e0484c94c2200a407
__hidehalo <tianchen_cc@yeah.net>
2023-02-10 14:27:19 +0800

    test

--- examples/demo/cluster/web/starx-wsclient.js
+++ examples/demo/cluster/web/starx-wsclient.js
@@ -441,8 +441,8 @@
   };
 
   var handshake = function(data) {
-    console.debug(new TextDecoder().decode(data));
-    data = JSON.parse(new TextDecoder().decode(data));
+    console.debug(Package.decode(data));
+    data = Package.decode(data);
     if(data.code === RES_OLD_CLIENT) {
       starx.emit('error', 'client version not fullfill');
       return;
